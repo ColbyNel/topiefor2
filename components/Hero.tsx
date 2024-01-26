f8ca30caf3c0fe127b88ab4f2d7ef6ece9f074cb
Colbyn Nel <colbynnel@gmail.com>
2024-01-26 15:27:22 +0200

    new fonts

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,7 +28,7 @@ const Hero = () => {
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+            <h1 className="font-feature text-4xl font-bold tracking-tight text-white sm:text-6xl">
               To Pie For
             </h1>
             <p className="mt-6 text-lg leading-8 text-white">
